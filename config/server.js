e5497047b20f8cbb6422355ef85a72c3f0a24b6b
Quốc Huy <quochuy170598@gmail.com^Cgit config --global user.email quochuy170598@gmail.com>
2021-03-29 14:56:27 +0700

    fist commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e50cd5972a67770d9482eb74b137c976'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fa534e3a-3a6a-4fc8-9e4d-6a624ada8e78'
+};
