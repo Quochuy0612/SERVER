edb03bbc19cf98f2ebc4a274de1d33fc580b759a
Quốc Huy <quochuy170598@gmail.com^Cgit config --global user.email quochuy170598@gmail.com>
2021-03-31 16:12:52 +0700

    3 commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,27 @@
-// module.exports = ({ env }) => ({
-//     upload: {
-//       provider: 'cloudinary',
-//       providerOptions: {
-//         cloud_name: env('CLOUDINARY_NAME'),
-//         api_key: env('CLOUDINARY_KEY'),
-//         api_secret: env('CLOUDINARY_SECRET'),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-//   });
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'dtpl7inip',
+      api_key: '562387952174729',
+      api_secret: 'E8HHVz52ucH8Fr9UcuRffju6NRU',
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+  // ...
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: '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',
+    },
+    settings: {
+      defaultFrom: 'thecoffeestudied@gmail.com',
+      defaultReplyTo: 'thecoffeestudied@gmail.com',
+      testAddress: 'quochuy170598@gmail.com',
+    },
+  },
+});
