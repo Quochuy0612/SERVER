bf94362f7929cc28f77fe4454f3ff05c7bc2ebd8
Quốc Huy <quochuy170598@gmail.com^Cgit config --global user.email quochuy170598@gmail.com>
2021-05-01 21:43:56 +0700

    push

--- api/class/controllers/class.js
+++ api/class/controllers/class.js
@@ -1,8 +1,26 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async update(ctx) {
+        const { id } = ctx.params;
+
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.class.update({ id }, {
+                $set: { "lecturers": req.body._id }
+            });
+            console.log(entity)
+        } else {
+            entity = await strapi.services.class.update({ id }, ctx.request.body);
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models.class });
+    },
+};
